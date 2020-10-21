d6ac7db6cab195e9c0a56f5a7fe970e82b2d0da2
bujhmt <bujhmt99999@gmail.com>
2020-10-21 16:19:33 +0300

    Prod controller 2

--- lab2/controllers/productController.py
+++ lab2/controllers/productController.py
@@ -15,22 +15,42 @@ class ProductController(object):
             print("Connection error! ", err)
 
     def getAll(self, page: int, per_page: int):
-        items = [Product]
+        items = []
         page -= 1
         self.cursor.execute(f'SELECT {Product().getKeys()} FROM "Product" LIMIT {per_page} OFFSET {page * per_page}')
         records = self.cursor.fetchall()
         for record in records:
             tmpItem = Product()
             tmpItem.parse(record)
-            print(tmpItem.getValues())
             items.append(tmpItem)
 
-        import copy
-        return copy.deepcopy(items)
+        return items
+
+    def add(self, *args):
+        try:
+            newEntity: Product = Product()
+            if len(args) > 0 and isinstance(args[0], Product):
+                newEntity = args[0]
+            else:
+                newEntity.fill()
+
+            if newEntity.isFull():
+                print(f'INSERT INTO "Product" ({newEntity.getKeys()}) VALUES ({newEntity.getValues()}) RETURNING id')
+                self.cursor.execute(f'INSERT INTO db_labs.public."Product" ({newEntity.getKeys()}) '
+                                    f'VALUES ({newEntity.getValues()}) RETURNING id')
+                return int(self.cursor.fetchone()[0])
+        except Exception as err:
+            print("Add error! ", err)
+        return False
+
 
 test = ProductController()
 products = test.getAll(1, 10)
 
-print( products[0].name)
-for product in products:
-    print(product.name)
+pr = Product()
+pr.name = "'igor'"
+pr.cost = 200
+pr.manufacturer = "'frnl'"
+pr.manufacture_date = "'2002-03-04'"
+pr.category = 1
+print(test.add(pr))

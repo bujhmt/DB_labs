b5276aa6e10721637d6867e4461c1507b5069203
bujhmt <bujhmt99999@gmail.com>
2020-10-24 16:53:08 +0300

    CRUD Product

--- lab2/database.py
+++ lab2/database.py
@@ -4,7 +4,7 @@ import sys
 sys.path.append('../')
 import utils.jsonReader
 
-def getCursor():
+def getConn():
     config = utils.jsonReader.JsonReader(os.getcwd()).getJsonObject('./../config.json')
     return psycopg2.connect(dbname=config['dbname'], user=config['user'],
-                            password=config['password'], host=config['host']).cursor()
+                            password=config['password'], host=config['host'])

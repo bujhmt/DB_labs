19e47351afd5681024ad1ea7de4add3eb499ba47
bujhmt <bujhmt99999@gmail.com>
2020-10-24 16:53:00 +0300

    CRUD Product

--- lab2/controllers/productController.py
+++ lab2/controllers/productController.py
@@ -9,21 +9,24 @@ class ProductController(object):
 
     def __init__(self):
         try:
-            self.cursor = db.getCursor()
+            self.conn = db.getConn()
+            self.cursor = self.conn.cursor()
             if self.cursor is None: raise Exception('No connection. Please, check your config.json or Postgre server')
         except Exception as err:
             print("Connection error! ", err)
 
     def getAll(self, page: int, per_page: int):
         items = []
-        page -= 1
-        self.cursor.execute(f'SELECT {Product().getKeys()} FROM "Product" LIMIT {per_page} OFFSET {page * per_page}')
-        records = self.cursor.fetchall()
-        for record in records:
-            tmpItem = Product()
-            tmpItem.parse(record)
-            items.append(tmpItem)
-
+        try:
+            page -= 1
+            self.cursor.execute(f'SELECT {Product().getKeys()} FROM "Product" LIMIT {per_page} OFFSET {page * per_page}')
+            records = self.cursor.fetchall()
+            for record in records:
+                tmpItem = Product()
+                tmpItem.parse(record)
+                items.append(tmpItem)
+        except Exception as err:
+            print("Get error! ", err)
         return items
 
     def add(self, *args):
@@ -31,26 +34,53 @@ class ProductController(object):
             newEntity: Product = Product()
             if len(args) > 0 and isinstance(args[0], Product):
                 newEntity = args[0]
-            else:
-                newEntity.fill()
+            else: newEntity.fill()
 
             if newEntity.isFull():
                 print(f'INSERT INTO "Product" ({newEntity.getKeys()}) VALUES ({newEntity.getValues()}) RETURNING id')
                 self.cursor.execute(f'INSERT INTO db_labs.public."Product" ({newEntity.getKeys()}) '
                                     f'VALUES ({newEntity.getValues()}) RETURNING id')
+                self.conn.commit()
                 return int(self.cursor.fetchone()[0])
         except Exception as err:
             print("Add error! ", err)
         return False
 
+    def getById(self, productId):
+        product = Product()
+        try:
+            if isinstance(productId, int): productId = str(productId)
+            if not isinstance(productId, str): raise Exception('Incorrect arguments')
+            self.cursor.execute(f'SELECT {product.getKeys()} from "Product" WHERE id = {productId}')
+            record = self.cursor.fetchone()
+            if record is not None: product.parse(record)
+            else: raise Exception(f'No entry with ID {productId} found')
+        except Exception as err:
+            print("Get by id error! ", err)
+        return product
 
-test = ProductController()
-products = test.getAll(1, 10)
+    def delete(self, productId):
+        try:
+            if isinstance(productId, int): productId = str(productId)
+            if not isinstance(productId, str): raise Exception('Incorrect arguments')
+            product = self.getById(productId)
+            self.cursor.execute(f'DELETE from "Product" WHERE id = {productId}')
+            self.conn.commit()
+            return product
+        except Exception as err:
+            print("Delete error! ", err)
+            return False
 
-pr = Product()
-pr.name = "'igor'"
-pr.cost = 200
-pr.manufacturer = "'frnl'"
-pr.manufacture_date = "'2002-03-04'"
-pr.category = 1
-print(test.add(pr))
+    def update(self, product: Product):
+        try:
+            queryStr = ''
+            keys = product.getKeys().split(',')
+            values = product.getValues().split(',')
+            for i in range(len(keys)):
+                queryStr += keys[i] + ' = ' + values[i] + ', '
+            self.cursor.execute(f'Update "Product" Set {queryStr[:-2]} Where id = {product.id}')
+            self.conn.commit()
+            return True
+        except Exception as err:
+            print("Update error! ", err)
+            return False
